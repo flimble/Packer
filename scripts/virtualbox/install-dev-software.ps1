280e1e91546a95583a21f847d22c6c0535015413
Simon Colmer <Simon.Colmer@totaljobsgroup.com>
2016-03-13 12:39:03 +0000

    Cleaned up dev software install

--- scripts/virtualbox/install-dev-software.ps1
+++ scripts/virtualbox/install-dev-software.ps1
@@ -1,5 +1,7 @@
 Write-Host "Installing developer software"
-choco install windows-sdk-8.1
+
+# Lets get this over with first
+choco install visualstudio2015professional # if you get error 1603 then you are missing some windows updates
 
 # Git things
 choco install git
@@ -15,10 +17,14 @@ choco install google-chrome-x64
 choco install firefox
 
 # MS Building Tools
+choco install windows-sdk-8.1
 choco install nuget.commandline
-choco install visualstudio2015professional
+
 choco install resharper-platform -y
-Start-Process -FilePath "C:\ProgramData\chocolatey\lib\resharper-platform\JetBrains.ReSharperUltimate.10.0.2.exe" -ArgumentList "/SpecificProductNames=ReSharper;dotTrace;dotCover;dotMemory;dotPeek /Silent=True" -Wait
+
+$resharperInstaller = Resolve-Path "$env:ChocolateyInstall\lib\resharper-platform\JetBrains.ReSharperUltimate.*.exe"
+Write-Host "Installing ReSharper Ultimate with lots of goodies: $resharperInstaller"
+Start-Process -FilePath "$resharperInstaller" -ArgumentList "/SpecificProductNames=ReSharper;dotTrace;dotCover;dotMemory;dotPeek /Silent=True" -Wait
 
 # SQL Tools
 choco install mssqlservermanagementstudio2014express
