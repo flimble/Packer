1692cbe33bdf238ce59a0bac16a3db7214cb550f
Simon Colmer <Simon.Colmer@totaljobsgroup.com>
2016-03-13 13:05:31 +0000

    Updating stage-4 hyperv naming convention

--- virtualbox-stage4-convert-to-hyperv.ps1
+++ virtualbox-stage4-convert-to-hyperv.ps1
@@ -1,10 +1,13 @@
 # Convert from OVF to hyper-v hard disk format
-$hyperVDir = "output-virtualbox-devdesktop\hyper-v-output\Virtual Hard Disks"
+$hyperVDir = "output-virtualbox-stage4-hyper-v"
 if(!(Test-Path $hyperVDir))
 {
+    Write-Host "Creating hyper-v output directory: $hyperVDir"
     mkdir $hyperVDir
 }
 
 $vboxDisk = Resolve-Path "output-virtualbox-stage3-dev-full-build\*.vmdk"
 $hyperVDisk = Join-Path $hyperVDir 'disk.vhd'
+
+Write-Host "Exporting hyper-v disk to $hyperVDisk"
 . "$env:programfiles\oracle\VirtualBox\VBoxManage.exe" clonehd $vboxDisk $hyperVDisk --format vhd --variant fixed
