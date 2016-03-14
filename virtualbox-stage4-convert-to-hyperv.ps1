bff15182de57df2e7f852cdbac6636c7ca59e565
Simon Colmer <Simon.Colmer@totaljobsgroup.com>
2016-03-14 19:40:43 +0000

    Fixed hyperv script...should it be compressed?

--- virtualbox-stage4-convert-to-hyperv.ps1
+++ virtualbox-stage4-convert-to-hyperv.ps1
@@ -6,8 +6,8 @@ if(!(Test-Path $hyperVDir))
     mkdir $hyperVDir
 }
 
-$vboxDisk = Resolve-Path "output-virtualbox-stage3-dev-full-build\*.vmdk"
+$vboxDisk = Resolve-Path "output-virtualbox-stage3\*.vmdk"
 $hyperVDisk = Join-Path $hyperVDir 'disk.vhd'
 
 Write-Host "Exporting hyper-v disk to $hyperVDisk"
-. "$env:programfiles\oracle\VirtualBox\VBoxManage.exe" clonehd $vboxDisk $hyperVDisk --format vhd --variant fixed
+. "$env:programfiles\oracle\VirtualBox\VBoxManage.exe" clonehd $vboxDisk $hyperVDisk --format vhd #--variant fixed
