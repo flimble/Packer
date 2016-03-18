09467f3c353243e97aa457a70fc3c1371fba35b7
Chris Small <chris.small@totaljobsgroup.com>
2016-03-18 15:56:47 +0000

    Partitions and check for DVD drive and change to G drive.

--- scripts/shared/create-d-partition.ps1
+++ scripts/shared/create-d-partition.ps1
@@ -1,4 +1,14 @@
 #################################################################################
+# Check for a DVD drive and change its drive letter
+#################################################################################
+$dvdDrive = Get-WmiObject win32_volume -filter DriveType=5
+if ($dvdDrive -and $dvdDrive.DriveLetter -eq "D:")
+{
+    $dvdDrive.DriveLetter = "G:"
+    $dvdDrive.Put()
+}
+
+#################################################################################
 # Create the D: volume, let it fill the the rest of the disk
 #################################################################################
 new-partition -DriveLetter D -DiskNumber 0 -UseMaximumSize
