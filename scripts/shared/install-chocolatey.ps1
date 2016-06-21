dff4c8e2d73452cb8e53599659618364886bb2ec
Simon C <simon.colmer@tjgdev.ds>
2016-06-21 22:53:13 +0100

    Ensuring specific version of choco is installed due to braking changes

--- scripts/shared/install-chocolatey.ps1
+++ scripts/shared/install-chocolatey.ps1
@@ -1,5 +1,6 @@
 #################################################################################
 # Install Chocolatey, stop the -y flag being needed for all "choco install"
 #################################################################################
+$env:chocolateyVersion = '0.9.9.12' # Install sepecific version as later 0.9.10.* brakes PSWindowsUpdate
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable --name allowGlobalConfirmation
