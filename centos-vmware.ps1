5aec0cfe798774ed86a33f707a1c739a894fa506
Chris Small <chris.small@totaljobsgroup.com>
2016-07-18 15:43:46 +0100

    Replaced Ubuntu with CentOS

--- centos-vmware.ps1
+++ centos-vmware.ps1
@@ -4,8 +4,9 @@ if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
 
 choco install packer -y
 
+# Centos template thanks to https://github.com/geerlingguy/packer-centos-6
 $start = get-date
-packer build -force ubuntu-vmware.json
+packer build -force --only=vmware-iso centos7.2.json
 
 $end = get-date
 $total = $end - $start
