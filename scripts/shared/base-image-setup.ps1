e00565db1dd3845eac9a854f0554fce118f30ade
Chris S <mrshrinkray@gmail.com>
2016-08-18 15:14:14 +0100

    Update base-image-setup.ps1

--- scripts/shared/base-image-setup.ps1
+++ scripts/shared/base-image-setup.ps1
@@ -6,6 +6,7 @@
 #################################################################################
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable --name allowGlobalConfirmation # stop the -y flag being needed for all "choco install"s
+choco feature disable --name checksumFiles # lots of packages have no checksums
 
 choco install pswindowsupdate
 Import-Module PSWindowsUpdate
