f5a09b8f10ff36eac7f722ec3ab42d7bb252c36c
Chris S <mrshrinkray@gmail.com>
2016-12-19 10:29:03 +0000

    aws: I got WinRM working!!!11 Teamcity agent edition too

--- aws.ps1
+++ aws.ps1
@@ -1,12 +1,27 @@
+# Check for chocolatey and install it
 if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
     iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
 }
 
-choco install packer -y
+# Check for packer and install it
+$hasPacker = chocolatey list -localonly | Select-String "packer"
+if ($hasPacker -eq $null)
+{
+    choco install packer -y
+}
 
+# Take a survey
 $aws_accesskey = $env:AWS_ACCESSKEY
 $aws_secretkey = $env:AWS_SECRETKEY
 $aws_ami_windows_id = "ami-771b4504"
+
+$aws_ami_name = $env:aws_ami_name
+$aws_security_group_id = $env:aws_security_group_id
+$aws_subnet_id = $env:aws_subnet_id
+$target = "aws-basewindows"
+
+Write-Host "Before starting, make sure you have created a key pair called 'packer'" -ForegroundColor Green
+Write-Host "This should be stored as packer.pem in this directory." -ForegroundColor Green
 
 if (!$aws_accesskey)
 {
@@ -27,22 +42,51 @@ if (!$aws_ami_id)
 {
     $aws_ami_id = $aws_ami_windows_id
 }
-$ami_name = Read-Host "Enter the name for your new AMI"
 
-$security_group_id = Read-Host "Enter a security group id"
-$subnet_id = Read-Host "Enter a subnet id"
+if (!$aws_ami_name)
+{
+    $aws_ami_name = Read-Host "Enter the name for your new AMI"
+    $env:aws_ami_name = $aws_ami_name
+}
+
+$aws_ami_description = Read-Host "Enter a description for your new AMI (Windows Server 2016 base)"
+if (!$aws_ami_description)
+{
+    $aws_ami_description = "Windows Server 2016 base"
+}
+
+$isTeamcity = Read-Host "Is this a teamcity agent (no)?"
+if ($isTeamcity -eq "yes" -or $isTeamcity -eq "y")
+{
+    $target = "aws-teamcity"
+}
+
+if (!$aws_security_group_id)
+{
+    $aws_security_group_id = Read-Host "Enter a security group id"
+    $env:aws_security_group_id = $aws_security_group_id
+}
+
+if (!$aws_subnet_id)
+{
+    $aws_subnet_id = Read-Host "Enter a subnet id"
+    $env:aws_subnet_id = $aws_subnet_id
+}
 
 $start = get-date
 
-packer build -force -only="aws-basewindows" `
-                    -var "ami_name=$ami_name" `
+$env:PACKER_LOG=1
+$env:PACKER_LOG_PATH="packerlog.txt"
+packer build -force -only="$target" `
+                    -var "ami_name=$aws_ami_name" `
+                    -var "ami_description=$aws_ami_description" `
                     -var "aws_accesskey=$aws_accesskey" `
                     -var "aws_secretkey=$aws_secretkey" `
-                    -var "security_group_id=$security_group_id" `
-                    -var "subnet_id=$subnet_id" `
+                    -var "security_group_id=$aws_security_group_id" `
+                    -var "subnet_id=$aws_subnet_id" `
                     -var "ami_id=$aws_ami_id" `
                     -var "associate_public_ip_address=false" `
-                     aws.json
+                     "aws.json"
 
 $end = get-date
 $total = $end - $start
---
+++ scripts/shared/teamcity-agent.ps1
@@ -0,0 +1,6 @@
+# Teamcity agent
+choco install googlechrome
+choco install chromedriver
+choco install nodejs.install
+choco install jdk8
+choco install visualstudio2015community
