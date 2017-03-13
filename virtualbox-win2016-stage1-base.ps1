4b6c39c18539058ec9814734bc9b24b0d121f6e0
Abubaker Bashir <abubaker.bashir@totaljobsgroup.com>
2017-03-13 15:30:02 +0000

    Added windows server 2016 virtualbox support

---
+++ virtualbox-win2016-stage1-base.ps1
@@ -0,0 +1,18 @@
+if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
+    iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
+}
+
+# specific versions due to bugs in 0.12.1
+choco install packer -y -version 0.12.0
+choco install VirtualBox -y -version 5.1.8.20161116
+
+# Extra logging
+$env:PACKER_LOG=1
+$env:PACKER_LOG_PATH="virtualbox-win2016-stage1.log"
+
+$start = get-date
+packer build -force -only="virtualbox-win2016-stage1-base" virtualbox_win2016.json
+
+$end = get-date
+$total = $end - $start
+Write-Host "Took $total to complete"
